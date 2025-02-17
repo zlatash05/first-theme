7fccf3c610e936fee499194d52ac4656dc6766e8
zlatash05 <shkabara.zlata.2005@gmail.com>
2025-02-17 22:25:22 +0500

    Solution6.java

--- Solution6.java
+++ Solution6.java
@@ -8,7 +8,7 @@ class Solution6 {
             List<Integer> row = new ArrayList<>(i + 1);
             for (int j = 0; j <= i; j++) {
                 if (j == 0 || j == i) {
-                    row.add(1);  // The first and last elements are always 1
+                    row.add(1); 
                 } else {
                     row.add(triangle.get(i - 1).get(j - 1) + triangle.get(i - 1).get(j));
                 }
